15528604ec1734bd7c56695ba825fa0e32541acc
mehmetayilmaz <77573251+mehmetayilmaz@users.noreply.github.com>
2023-06-07 16:24:14 +0300

    Add project files.

---
+++ Armut.WebAppbenim/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Armut.WebAppbenim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
